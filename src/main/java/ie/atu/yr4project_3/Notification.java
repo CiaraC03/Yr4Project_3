c73603c1ca83c745ea749c1b0be41d4398a89244
ciara <g00402357@atu.ie>
2024-12-29 17:15:07 +0000

    Added notification entities with userID which will link the task service, the message of the notification and the type of notification

--- src/main/java/ie/atu/yr4project_3/Notification.java
+++ src/main/java/ie/atu/yr4project_3/Notification.java
@@ -1,4 +1,25 @@
 package ie.atu.yr4project_3;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull(message = "ID cant be null")
+    private Long userId;
+    @NotBlank(message = "Message can't be blank")
+    private String message;
+    @NotBlank(message = "Notification can't be blank")
+    private String type;
 }
